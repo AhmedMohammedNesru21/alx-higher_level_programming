56ddb3cecc81a1ed50682d83838dc75780a1d2fc
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-10-18 01:51:48 +0000

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,81 +1,50 @@
 #!/usr/bin/python3
-"""Module is to solve the N-Queens challenge problem"""
-from sys import argv
+'''Module for N Queens problem.'''
 
 
-def checkspot(board, r, c):
-    n = len(board) - 1
-    if board[r][c]:
-        return 0
-    for row in range(r):
-        if board[row][c]:
-            return 0
-    i = r
-    j = c
-    while i > 0 and j > 0:
-        i -= 1
-        j -= 1
-        if board[i][j]:
-            return 0
-    i = r
-    j = c
-    while i > 0 and j < n:
-        i -= 1
-        j += 1
-        if board[i][j]:
-            return 0
-    return 1
+def isSafe(board, row, col):
+    '''Checks if position is safe from attack.
+    Args:
+        board: The board state.
+        row: The row to check.
+        col: The colum to check.
+    '''
+    for c in range(col):
+        if board[c] is row or abs(board[c] - row) is abs(c - col):
+            return False
+    return True
 
 
-def initboard(n=4):
-    b = []
-    for r in range(n):
-        b.append([0 for c in range(n)])
-    return b
-
-
-def findsoln(board, row):
-    for col in range(len(board)):
-        if checkspot(board, row, col):
-            board[row][col] = 1
-            if row == len(board) - 1:
-                print(convtosoln(board))
-                board[row][col] = 0
-                continue
-            if findsoln(board, row + 1):
-                return board
-            else:
-                board[row][col] = 0
-    return None
-
-
-def convtosoln(board):
-    soln = []
+def checkBoard(board, col):
+    '''Checks the board state column by column using backtracking.
+    Args:
+        board: The board state.
+        col: The current colum to check.
+    '''
     n = len(board)
-    for r in range(n):
-        for c in range(n):
-            if board[r][c]:
-                soln.append([r, c])
-    return soln
+    if col is n:
+        print(str([[c, board[c]] for c in range(n)]))
+        return
 
-
-def nqueens(n=4):
-    for col in range(n):
-        board = initboard(n)
-        board[0][col] = 1
-        findsoln(board, 1)
-
+    for row in range(n):
+        if isSafe(board, row, col):
+            board[col] = row
+            checkBoard(board, col + 1)
 
 if __name__ == "__main__":
-    if len(argv) != 2:
+    import sys
+
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
+        sys.exit(1)
+    n = 0
     try:
-        n = int(argv[1])
+        n = int(sys.argv[1])
     except:
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
     if n < 4:
         print("N must be at least 4")
-        exit(1)
-    nqueens(n)
+        sys.exit(1)
+    board = [0 for col in range(n)]
+    checkBoard(board, 0)
