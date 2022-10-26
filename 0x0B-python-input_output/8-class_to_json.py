1d4af6a20bea6053a5e45460452827f05deb4ad1
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-10-26 00:16:02 +0000

    Task 0-14

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-
+'''
+Write a function that creates an
+Object from a JSON file
+'''
 
 import json
 
 
 def load_from_json_file(filename):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return json.load(f)
+    '''
+    Create object from JSON file
+    '''
+    if filename is None:
+        return
+    with open(filename, 'r', encoding='utf-8') as f:
+        json_var = json.load(f)
+        return json_var
