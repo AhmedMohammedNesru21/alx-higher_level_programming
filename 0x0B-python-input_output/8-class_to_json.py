dc6b4115c2d73d152d0bde50128fce3acd8ed64f
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2022-10-26 00:06:49 +0000

    Task 0-14

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" creates an Object from a JSON file """
+
+
 import json
 
 
 def load_from_json_file(filename):
-    """ creates an Object from a JSON file """
-    with open(filename, 'r', encoding='utf8') as f:
-        obj = json.load(f)
-        return obj
+    with open(filename, "r", encoding="UTF-8") as f:
+        return json.load(f)
