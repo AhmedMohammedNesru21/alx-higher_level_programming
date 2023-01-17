93ccdcc2f5257508f0494c608418cdb49585d093
AhmedMohammedNesru21 <ahmed.comp10@gmail.com>
2023-01-17 19:43:58 +0000

    -Task 0-11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-# task adavance 11
-import sys
-import requests
+"""Python script that lists list 10 commits (from the most recent
+to oldest) of the repository rails by the user rails."""
 
-if __name__ == '__main__':
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],
-                                                              sys.argv[1])
-    req = requests.get(url)
-    content = req.json()
-    cont = 0
-    for item in content:
-        if cont < 10:
-            print(item['sha'] + ': ' + item['commit']['author']['name'])
-            cont += 1
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+          .format(argv[2], argv[1])
+
+    request = requests.get(url)
+    commits = request.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
